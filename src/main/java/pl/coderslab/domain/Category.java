389d3fdb793e2e30af28ce792ce1333448f2f641
Lukasz Citko <citko.lukash@gmail.com>
2024-08-01 20:52:45 +0000

    initialization of project

---
+++ src/main/java/pl/coderslab/domain/Category.java
@@ -0,0 +1,26 @@
+package pl.coderslab.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {return name;}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
